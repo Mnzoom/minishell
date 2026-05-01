8b60404c767b6a6c43fc0c8d4b332190e7f9391e
Aweh <anton@AwehWindows.localdomain>
2026-05-01 19:01:04 +0200

    Fix & Reworks. Piping fd race condition & using ft_calloc for safely allocating empty expansion

--- mini_exec/exec_pipe.c
+++ mini_exec/exec_pipe.c
@@ -6,7 +6,7 @@
 /*   By: thantoni <thantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 18:55:34 by clementngoi       #+#    #+#             */
-/*   Updated: 2026/04/30 20:03:25 by thantoni         ###   ########.fr       */
+/*   Updated: 2026/05/01 18:57:59 by thantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,43 +16,49 @@
 
 extern int g_lastsignal;
 
-int	exec_pipe(t_cmd *cmd_list, t_env *env_list)
+int exec_pipe(t_cmd *m_cmd_list, t_env *m_env_list)
 {
 	int		pipe_fd[2];
 	int		fd_in;
 	pid_t	last_pid;
 	int		status;
 
-	fd_in = STDERR_FILENO;
-	while (cmd_list)
+	fd_in = dup(STDIN_FILENO);
+	while (m_cmd_list)
 	{
-		if (pipe(pipe_fd) == -1)
-			perror("pipe");
+		if (m_cmd_list->next)
+		{
+			if (pipe(pipe_fd) == -1)
+				perror("pipe");
+		}
 		last_pid = fork();
 		if (last_pid == -1)
 			perror("fork");
+
 		if (last_pid == 0)
 		{
 			dup2(fd_in, STDIN_FILENO);
-			if (fd_in != STDERR_FILENO)
-				close(fd_in);
-			if (cmd_list->next)
+			close(fd_in);
+			if (m_cmd_list->next)
+			{
 				dup2(pipe_fd[1], STDOUT_FILENO);
-			close(pipe_fd[0]);
-			close(pipe_fd[1]);
-			exec_child_process(cmd_list, env_list);
+				close(pipe_fd[0]);
+				close(pipe_fd[1]);
+			}
+			exec_child_process(m_cmd_list, m_env_list);
 			exit(1);
 		}
 		else
 		{
-			if (fd_in != 0)
-				close(fd_in);
-			close(pipe_fd[1]);
-			fd_in = pipe_fd[0];
-			cmd_list = cmd_list->next;
+			close(fd_in);
+			if (m_cmd_list->next)
+			{
+				close(pipe_fd[1]);
+				fd_in = pipe_fd[0];
+			}
+			m_cmd_list = m_cmd_list->next;
 		}
 	}
-	close(fd_in);
 	waitpid(last_pid, &status, 0);
 	if (WIFEXITED(status))
 		g_lastsignal = WEXITSTATUS(status);
